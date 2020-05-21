172a4c3d2663122a4c91bfd6f6e4851535877b7d
beata <beata@emc2wave.com>
2020-05-20 19:15:12 -0700

    add const income, expense

--- script.js
+++ script.js
@@ -39,7 +39,17 @@ function updateValue() {
 
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
-  console.log(total);
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  console.log(expense);
 }
 
 // Init app
