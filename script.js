9d58fae121dd7120294f54342590dd20dbc1a934
beata <beata@emc2wave.com>
2020-05-20 19:23:44 -0700

    add balance, money_plus, money_minus innerText

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const balance = document.getElementById('balance');
 const money_plus = document.getElementById('money-plus');
-const money_minus = document.getElementById('money_minus');
+const money_minus = document.getElementById('money-minus');
 const list = document.getElementById('list');
 const form = document.getElementById('form');
 const text = document.getElementById('text');
@@ -49,7 +49,9 @@ function updateValue() {
     -1
   ).toFixed(2);
 
-  console.log(expense);
+  balance.innerText = `$${total}`;
+  money_plus.innerText = `+$${income}`;
+  money_minus.innerText = `-$${expense}`;
 }
 
 // Init app
